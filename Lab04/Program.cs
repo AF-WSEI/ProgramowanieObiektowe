c17392a1c76123a8bb58cdb164b257811ac155b7
AF-WSEI <adam.felisiak@microsoft.wsei.edu.pl>
2026-03-17 13:42:07 +0100

    Adding Lab04

---
+++ Lab04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+}
+
+public class IntNode
+{
+    public int Value { get; set; }
+    public IntNode Next { get; set; }
+}
