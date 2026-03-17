ba2a9bdaa4a3e3f40850a7f5c788575e0d975bee
AF-WSEI <adam.felisiak@microsoft.wsei.edu.pl>
2026-03-17 14:01:25 +0100

    Added generic implementation for IntNode to make it universal. Added code in Main to show how the Nodes work.

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -4,11 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
+        Node<int> intNode = new Node<int>()
+        {
+            Value = 10,
+            Next = new Node<int>()
+            {
+                Value = 5,
+                Next = new Node<int>()
+                {
+                    Value = 50,
+                    Next = null
+                }
+            }
+        };
+        Node<int>.Print(intNode);
+
+        Console.WriteLine();
+
+        Node<string> stringNode = new Node<string>()
+        {
+            Value = "Grace",
+            Next = new Node<string>()
+            {
+                Value = "Ada",
+                Next = new Node<string>()
+                {
+                    Value = "Leon",
+                    Next = null
+                }
+            }
+        };
+        Node<string>.Print(stringNode);
     }
 }
 
 public class IntNode
 {
     public int Value { get; set; }
-    public IntNode Next { get; set; }
+    public IntNode? Next { get; set; }
+
+    public static void Print(IntNode head)
+    {
+        IntNode temp = head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Value);
+            temp = temp.Next;
+        }
+    }
+}
+
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T>? Next { get; set; }
+
+    public static void Print(Node<T> head)
+    {
+        Node<T>? temp = head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Value);
+            temp = temp.Next;
+        }
+    }
 }
