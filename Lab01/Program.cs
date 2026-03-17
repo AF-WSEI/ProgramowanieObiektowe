f5acdde532f3c7cc78a668542100c3bf9f725a72
AF-WSEI <adam.felisiak@microsoft.wsei.edu.pl>
2026-03-17 13:38:13 +0100

    Adding missing files in Lab01

---
+++ Lab01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using Lab01;
+
+int count = 0;
+count = int.Parse(Console.ReadLine());
+for (int i = 0; i < count; i++)
+{
+    Console.WriteLine("Hello, World!");
+}
+
+
+Console.WriteLine("Klasa User");
+User user = new User("1234")
+{
+    Id = Guid.NewGuid(),
+    FirstName = "John",
+    LastName = "Cena"
+};
+
+Console.WriteLine("Klasa Money");
+var amount = new Money
+{
+    Value = 34.6m,
+    Currency = Currency.EUR
+};
+Console.WriteLine(amount);
